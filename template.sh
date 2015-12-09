beceaa33f0b494a4f73c771d7bed0058ce7a4105
Jelle Derksen <jelle@jellederksen.nl>
2015-12-09 14:58:37 +0100

    template
    template

---
+++ template.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+#Short Description Of Change
+#
+#Copyright 2015 Jelle Derksen GNU GPL V3
+#Author Jelle Derksen
+#Contact jelled@jellederksen.nl
+#Website www.jellederksen.nl
+#
+#Long description of change on the host this script is executed on.
+#
+#Example: exclude host from script.
+#exclude_hosts[0]='foo.bar.com'
+#exclude_hosts[1]='192.168.0.1'
+#
+#Example: exclude element on host from script.
+#exclude_element_on_host='foo.bar.com,users,1'
+#exclude_element_on_host='192.16.0.3,users,0'
+#
+#Example: array with change you want to make.
+#array_change[0]='Change you want to make.'
+
+#Script settings change to suit your needs.
+exclude_host[0]=''
+exclude_element_on_host[0]=''
+array_change[0]='Change you want to make.'
+
+#Do not edit below this point.
+#Script checks.
+if [[ ! "$USER" = 'root' ]]; then
+	echo 'need root privileges'
+	exit 1
+fi
+
+if [[ ! "$(uname)" = 'Linux' ]]; then
+	echo 'OS not Linux.'
+	exit 2
+fi
+
+if [[ -n "$exclude_host" ]]; then
+	for x in "${exclude_host[@]}"; do
+		h="$(echo "$x" | awk -F, '{print $1}')"
+		if [[ "$h" = "$(hostname)" ]]; then
+			exit 0
+		elif [[ "$h" = "$(ip addr show | grep -F -o "$h")" ]]; then
+			exit 0
+		fi
+	done
+fi
+
+if [[ -n "$exclude_element_on_host" ]]; then
+	for x in "${exclude_element_on_host[@]}"; do
+		h="$(echo "$x" | awk -F, '{print $1}')"
+		a="$(echo "$x" | awk -F, '{print $2}')"
+		e="$(echo "$x" | awk -F, '{print $3}')"
+		if [[ -z "$h" || -z "$a" || -z "$e" ]]; then
+			echo "$x variable incorrect"
+			exit 3
+		fi
+		if [[ "$(hostname)" = "$h" ]]; then
+			unset "${a}[${e}]"
+		elif [[ "$h" = "$(ip addr show | grep -F -o "$h")" ]]; then
+			unset "${a}[${e}]"
+		fi
+	done
+fi
+
+#Main code.
+
+#Perform actions with elements in array_change.
+
+exit 0
