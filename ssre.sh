b5b1e13d661b44d031399d5274e7fb4e27ba480e
Jelle Derksen <jelle@jellederksen.nl>
2015-12-09 14:48:30 +0100

    Ssh Script Remote Execute
    Ssh Script Remote Execute

--- ssre.sh
+++ ssre.sh
@@ -30,20 +30,27 @@ remote_host[0]=''
 ssh_user=''
 
 #Do not edit below this point.
+#Script checks.
+if [[ -z "$remote_host" || -z "$ssh_user" ]]; then
+	echo 'ssh_user and remote_host not set'
+	exit 1
+fi
+
 if [[ ! -r "$1" ]]; then
         echo "Enter the script name you want to execute." >&2
-        exit 1
+        exit 2
 else
         base64_script="$(base64 -w0 "$1")"
 fi
 
+#Main code.
 for x in "${remote_host[@]}"; do
 	echo "Ssh to $remote_host and executing script $1."
 	ssh -o StrictHostKeyChecking=no "$ssh_user@$x" \
 "echo $base64_script | base64 -d | sudo bash"
 	if [[ ! "$?" -eq '0' ]]; then
 		echo "Someting went wrong on the host $x."
-		exit 1
+		exit 3
 	fi
 done
 
