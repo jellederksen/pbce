487108263b956ebb1566c269c126e777d2a6ae7a
Jelle Derksen <jelle@jellederksen.nl>
2015-12-13 22:40:39 +0100

    functions.sh
    functions.sh

---
+++ functions.sh
@@ -0,0 +1,39 @@
+#Do not edit below this point.
+#Script checks.
+if [[ ! "$USER" = 'root' ]]; then
+	echo 'need root privileges'
+	exit 1
+fi
+
+if [[ ! "$(uname)" = 'Linux' ]]; then
+	echo 'OS not Linux.'
+	exit 2
+fi
+
+if [[ -n "$exclude_host" ]]; then
+	for x in "${exclude_host[@]}"; do
+		h="$(echo "$x" | awk -F, '{print $1}')"
+		if [[ "$h" = "$(hostname)" ]]; then
+			exit 0
+		elif [[ "$h" = "$(ip addr show | grep -F -o "$h")" ]]; then
+			exit 0
+		fi
+	done
+fi
+
+if [[ -n "$exclude_element_on_host" ]]; then
+	for x in "${exclude_element_on_host[@]}"; do
+		h="$(echo "$x" | awk -F, '{print $1}')"
+		a="$(echo "$x" | awk -F, '{print $2}')"
+		e="$(echo "$x" | awk -F, '{print $3}')"
+		if [[ -z "$h" || -z "$a" || -z "$e" ]]; then
+			echo "$x variable incorrect"
+			exit 3
+		fi
+		if [[ "$(hostname)" = "$h" ]]; then
+			unset "${a}[${e}]"
+		elif [[ "$h" = "$(ip addr show | grep -F -o "$h")" ]]; then
+			unset "${a}[${e}]"
+		fi
+	done
+fi
