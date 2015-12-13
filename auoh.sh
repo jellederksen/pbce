69c23f3c7b30f3acff2356a68fdbe0249affc79d
Jelle Derksen <jelle@jellederksen.nl>
2015-12-13 21:44:29 +0100

    Add User Ssh Public-key
    Add User Ssh Public-key

--- auoh.sh
+++ auoh.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #
-#Add Users On Host
+#Add User Ssh Public-key
 #
 #Copyright 2015 Jelle Derksen GNU GPL V3
 #Author Jelle Derksen
 #Contact jelled@jellederksen.nl
 #Website www.jellederksen.nl
 #
-#All users in the array "$users[@]" will be added to the host this
-#script is executed on. You can execute this script on multiple hosts
-#by executing this script with the ssre.sh script. The correct syntax
-#for the "$users[@]" variable can be seen in the example. Please mind
-#the quotes arround the full_name part. You will need the quotes when
-#using a space between the first and last name.
+#All public-keys in the "${user_pubkey[@]}" array will be added to the users
+#authorized_keys file on the host this script is executed on. You can execute
+#this script on multiple hosts by executing this script with the ssre.sh
+#script. The correct syntax for the "$user_pubkey[@]" array can be seen in
+#the example. Please mind the quotes around the public-key part.
 #
 #Example: exclude host from script.
 #exclude_hosts[0]='foo.bar.com'
@@ -22,14 +21,13 @@
 #exclude_element_on_host='foo.bar.com,users,1'
 #exclude_element_on_host='192.16.0.3,users,0'
 #
-#Example: add user accounts.
-#users[0]="group_name,full_name,'home_directory',prefered_shell,account_name"
-#users[1]="jelle,'Jelle Derksen',/home/jelle,/bin/ksh,jelle"
+#Example: add public-key to users authorized_keys file.
+#user_pubkey[0]="user_name,'ssh_public_key','ssh_public_key'"
 
 #Script settings change to suit your needs.
 exclude_host[0]=''
 exclude_element_on_host[0]=''
-user[0]="jelle,'Jelle Derksen',/home/jelle,/bin/ksh,jelle"
+user_pubkey[0]=""
 
 #Do not edit below this point.
 #Script checks.
@@ -60,7 +58,7 @@ if [[ -n "$exclude_element_on_host" ]]; then
 		a="$(echo "$x" | awk -F, '{print $2}')"
 		e="$(echo "$x" | awk -F, '{print $3}')"
 		if [[ -z "$h" || -z "$a" || -z "$e" ]]; then
-			echo "$x variable incorrect"
+			echo "$x variable incorrect."
 			exit 3
 		fi
 		if [[ "$(hostname)" = "$h" ]]; then
@@ -72,27 +70,40 @@ if [[ -n "$exclude_element_on_host" ]]; then
 fi
 
 #Main code.
-for u in "${user[@]}"; do
-	g="$(echo $u | awk -F, '{print $1}')"
-	n="$(echo $u | awk -F, '{print $2}')"
-	h="$(echo $u | awk -F, '{print $3}')"
-	s="$(echo $u | awk -F, '{print $4}')"
-	a="$(echo $u | awk -F, '{print $5}')"
-	if [[ -z "$g" || -z "$n" || -z "$h" || -z "$s" || -z "$a" ]]; then
-		echo "$u variable incorrect."
+for x in "${user_pubkey[@]}"; do
+	u="$(echo "$x" | awk -F, '{print $1}')"
+	k="$(echo "$x" | awk -F, '{print $2}')"
+	#Remove quotes arround public-key.
+	eval k="$k"
+	h="$(grep "$u" /etc/passwd | awk -F: '{print $6}')"
+	g="$(grep "$u" /etc/passwd | awk -F: '{print $4}')"
+	if [[ -z "$u" || -z "$k" || -z "$h" || -z "$g" ]]; then
+		echo "$x variable incorrect."
 		exit 4
 	fi
-	if id "$a" > /dev/null 2>&1; then
-		echo "$a already on $(hostname)."
+	if ! id "$u" > /dev/null 2>&1; then
+		echo "User $u does not exist."
 		continue
 	fi
-	if useradd -g "$g" -c "$n" -d "$h" -s "$s" "$a"; then
-		echo "$a added on host $(hostname)."
-	else
-		echo "Failed to add $a on host $(hostname)."
+	if [[ ! -d "${h}" ]]; then
+		echo "$u has no home directory."
 		exit 5
+	elif [[ ! -d "${h}/.ssh" ]]; then
+		mkdir "${h}/.ssh"
+		echo "#${u}'s SSH public-key" > "${h}/.ssh/authorized_keys"
+		echo "$k" >> "${h}/.ssh/authorized_keys"
+		chown -R "$u":"$g" "${h}/.ssh"
+	else
+		if grep -F "$k" "${h}/.ssh/authorized_keys" > \
+		/dev/null 2>&1; then
+			echo "Key for $u already in authorized_keys file."
+			continue
+		fi
+		echo "#${u}'s SSH public-key" >> "${h}/.ssh/authorized_keys"
+		echo "$k" >> "${h}/.ssh/authorized_keys"
+		chown "$u":"$g" "${h}/.ssh/authorized_keys"
 	fi
 done
 
-echo "All accounts added on host $(hostname)."
+echo "All keys are added to $(hostname)."
 exit 0
