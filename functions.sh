40fc076bfead1a13da75d1a2efed2c3be99162e4
Jelle Derksen <jelle@jellederksen.nl>
2015-12-15 12:56:07 +0100

    functions.sh
    functions.sh

--- functions.sh
+++ functions.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
 
-if [[ ! "$USER" = 'root' ]]; then
-	echo 'need root privileges'
-	exit 1
-fi
+check_root() {
+	if [[ ! "$USER" = 'root' ]]; then
+		echo 'need root privileges'
+		exit 1
+	fi
+}
 
-if [[ ! "$(uname)" = 'Linux' ]]; then
-	echo 'OS not Linux.'
-	exit 2
-fi
+check_os() {
+	if [[ ! "$(uname)" = 'Linux' ]]; then
+		echo 'OS not Linux.'
+		exit 2
+	fi
+}
 
-if [[ -n "$exclude_host" ]]; then
-	for x in "${exclude_host[@]}"; do
-		h="$(echo "$x" | awk -F, '{print $1}')"
-		if [[ "$h" = "$(hostname)" ]]; then
-			exit 0
-		elif [[ "$h" = "$(ip addr show | grep -F -o "$h")" ]]; then
-			exit 0
-		fi
-	done
-fi
+exclude_host() {
+	if [[ -n "$exclude_host" ]]; then
+		for x in "${exclude_host[@]}"; do
+			h="$(echo "$x" | awk -F, '{print $1}')"
+			if [[ "$h" = "$(hostname)" ]]; then
+				exit 0
+			elif [[ "$h" = "$(ip addr show | grep -F -o "$h")" ]]; then
+				exit 0
+			fi
+		done
+	fi
+}
 
-if [[ -n "$exclude_element_on_host" ]]; then
-	for x in "${exclude_element_on_host[@]}"; do
-		h="$(echo "$x" | awk -F, '{print $1}')"
-		a="$(echo "$x" | awk -F, '{print $2}')"
-		e="$(echo "$x" | awk -F, '{print $3}')"
-		if [[ -z "$h" || -z "$a" || -z "$e" ]]; then
-			echo "$x variable incorrect"
-			exit 3
-		fi
-		if [[ "$(hostname)" = "$h" ]]; then
-			unset "${a}[${e}]"
-		elif [[ "$h" = "$(ip addr show | grep -F -o "$h")" ]]; then
-			unset "${a}[${e}]"
-		fi
-	done
-fi
-
-echo "Done making changes on $(hostname)."
-exit 0
+exclude_element() {
+	if [[ -n "$exclude_element_on_host" ]]; then
+		for x in "${exclude_element_on_host[@]}"; do
+			h="$(echo "$x" | awk -F, '{print $1}')"
+			a="$(echo "$x" | awk -F, '{print $2}')"
+			e="$(echo "$x" | awk -F, '{print $3}')"
+			if [[ -z "$h" || -z "$a" || -z "$e" ]]; then
+				echo "$x variable incorrect"
+				exit 3
+			fi
+			if [[ "$(hostname)" = "$h" ]]; then
+				unset "${a}[${e}]"
+			elif [[ "$h" = "$(ip addr show | grep -F -o "$h")" ]]; then
+				unset "${a}[${e}]"
+			fi
+		done
+	fi
+}
