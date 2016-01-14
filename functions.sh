fb1a6769404ded11fe1b796741d34e879f7f47bc
Jelle Derksen <jelle@jellederksen.nl>
2016-01-14 12:48:03 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 #Functions for pbce scripts
 #
 #Copyright 2015 Jelle Derksen GNU GPL V3
@@ -17,38 +19,4 @@ check_os() {
 		echo 'OS not Linux.'
 		exit 2
 	fi
-}
-
-exclude_host() {
-	if [[ -z "$exclude_host" ]]; then
-		return 0
-	fi
-	for x in "${exclude_host[@]}"; do
-		h="$(echo "$x" | awk -F, '{print $1}')"
-		if [[ "$h" = "$(hostname)" ]]; then
-			exit 0
-		elif [[ "$h" = "$(ip addr show | grep -F -o "$h")" ]]; then
-			exit 0
-		fi
-	done
-}
-
-exclude_element() {
-	if [[ "$exclude_element_on_host" ]]; then
-		return 0
-	fi
-	for x in "${exclude_element_on_host[@]}"; do
-		h="$(echo "$x" | awk -F, '{print $1}')"
-		a="$(echo "$x" | awk -F, '{print $2}')"
-		e="$(echo "$x" | awk -F, '{print $3}')"
-		if [[ -z "$h" || -z "$a" || -z "$e" ]]; then
-			echo "$x variable incorrect"
-			exit 3
-		fi
-		if [[ "$(hostname)" = "$h" ]]; then
-			unset "${a}[${e}]"
-		elif [[ "$h" = "$(ip addr show | grep -F -o "$h")" ]]; then
-			unset "${a}[${e}]"
-		fi
-	done
 }
