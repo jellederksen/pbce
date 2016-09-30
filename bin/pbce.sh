b49839c65cad03cb8679cb315c2cdfa8bb3e3ab9
Jelle Derksen <jelle@jellederksen.nl>
2016-09-30 09:48:45 +0200

    Update Lisence
    Update Lisence

--- bin/pbce.sh
+++ bin/pbce.sh
@@ -23,7 +23,6 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 #
-#
 #Poor Basterds Command Executer execute jobs and commands on remote Linux systems.
 
 #Script variables
